945fd1025a1477d692e2f0922867afef6bbcc38b
James Andrew Pohadi <jamesandrewpohadi@gmail.com>
2018-11-16 22:55:46 +0800

    first commit

---
+++ ventory/static/station/script.js
@@ -0,0 +1,185 @@
+// https://insights.stackoverflow.com/survey/2018/#technology-most-loved-dreaded-and-wanted-languages
+    var sample = [
+      /*{
+        language: 'Flight',
+        id:'flight',
+        value: 0,
+        color: '#000000'
+      },*/
+      {
+        language: 'Washing/Holding Area',
+        id:'holding',
+        value: 0,
+        color: '#00a2ee'
+      },
+      {
+        language: 'Packing Area',
+        id:'packing',
+        value: 0,
+        color: '#fbcb39'
+      }
+      
+    ];
+
+    var svg = d3.select('svg');
+    var svgContainer = d3.select('#container');
+    
+    var margin = 80;
+    var width = 1000 - 2 * margin;
+    var height = 600 - 2 * margin;
+
+    var chart = svg.append('g')
+      .attr('transform', `translate(${margin}, ${margin})`);
+
+    var xScale = d3.scaleBand()
+      .range([0, width])
+      .domain(sample.map((s) => s.language))
+      .padding(0.4)
+    
+    var yScale = d3.scaleLinear()
+      .range([height, 0])
+      .domain([0, 20000]);
+
+    // vertical grid lines
+    // const makeXLines = () => d3.axisBottom()
+    //   .scale(xScale)
+
+    var makeYLines = () => d3.axisLeft()
+      .scale(yScale)
+
+    chart.append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .call(d3.axisBottom(xScale));
+
+    chart.append('g')
+      .call(d3.axisLeft(yScale));
+
+    // vertical grid lines
+    // chart.append('g')
+    //   .attr('class', 'grid')
+    //   .attr('transform', `translate(0, ${height})`)
+    //   .call(makeXLines()
+    //     .tickSize(-height, 0, 0)
+    //     .tickFormat('')
+    //   )
+
+    chart.append('g')
+      .attr('class', 'grid')
+      .call(makeYLines()
+        .tickSize(-width, 0, 0)
+        .tickFormat('')
+      )
+
+    var barGroups = chart.selectAll()
+      .data(sample)
+      .enter()
+      .append('g')
+
+    barGroups
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('id',(g) => g.id)
+      .attr('x', (g) => xScale(g.language))
+      .attr('y', (g) => yScale(g.value))
+      .attr('height', (g) => height- yScale(g.value))
+      .attr('width', xScale.bandwidth())
+      .on('mouseenter', function (actual, i) {
+        d3.selectAll('.value')
+          .attr('opacity', 0)
+
+        d3.select(this)
+          .transition()
+          .duration(300)
+          .attr('opacity', 0.6)
+          .attr('x', (a) => xScale(a.language) - 5)
+          .attr('width', xScale.bandwidth() + 10)
+
+        const y = yScale(actual.value)
+
+        line = chart.append('line')
+          .attr('id', 'limit')
+          .attr('x1', 0)
+          .attr('y1', y)
+          .attr('x2', width)
+          .attr('y2', y)
+
+        barGroups.append('text')
+          .attr('class', 'divergence')
+          .attr('x', (a) => xScale(a.language) + xScale.bandwidth() / 2)
+          .attr('y', (a) => yScale(a.value) + 30)
+          .attr('fill', 'white')
+          .attr('text-anchor', 'middle')
+          .text((a, idx) => {
+            const divergence = (a.value - actual.value).toFixed(1)
+            
+            let text = ''
+            if (divergence > 0) text += '+'
+            text += `${divergence}`
+
+            return idx !== i ? text : '';
+          })
+
+      })
+      .on('mouseleave', function () {
+        d3.selectAll('.value')
+          .attr('opacity', 1)
+
+        d3.select(this)
+          .transition()
+          .duration(300)
+          .attr('opacity', 1)
+          .attr('x', (a) => xScale(a.language))
+          .attr('width', xScale.bandwidth())
+          .attr('y', (a) => yScale(a.value))
+          .attr('height', (a) => height- yScale(a.value))
+
+          chart.selectAll('#limit').remove()
+        chart.selectAll('.divergence').remove()
+        chart.selectAll('.value').remove()
+          barGroups 
+      .append('text')
+      .attr('class', 'value')
+      .attr('x', (a) => xScale(a.language) + xScale.bandwidth() / 2)
+      .attr('y', (a) => yScale(a.value) + 30)
+      .attr('text-anchor', 'middle')
+      .text((a) => `${a.value}`)
+      })
+
+    barGroups 
+      .append('text')
+      .attr('class', 'value')
+      .attr('x', (a) => xScale(a.language) + xScale.bandwidth() / 2)
+      .attr('y', (a) => yScale(a.value) + 30)
+      .attr('text-anchor', 'middle')
+      .text((a) => `${a.value}`)
+    
+    svg
+      .append('text')
+      .attr('class', 'label')
+      .attr('x', -(height / 2) - margin)
+      .attr('y', margin / 2.4)
+      .attr('transform', 'rotate(-90)')
+      .attr('text-anchor', 'middle')
+      .text('number of items (pcs)')
+
+    svg.append('text')
+      .attr('class', 'label')
+      .attr('x', width / 2 + margin)
+      .attr('y', height + margin * 1.7)
+      .attr('text-anchor', 'middle')
+      .text('Inventory')
+
+    svg.append('text')
+      .attr('class', 'title')
+      .attr('x', width / 2 + margin)
+      .attr('y', 40)
+      .attr('text-anchor', 'middle')
+      .text('')
+
+    svg.append('text')
+      .attr('class', 'source')
+      .attr('x', width - margin / 2)
+      .attr('y', height + margin * 1.7)
+      .attr('text-anchor', 'start')
+      .text('')
+  
---
+++ ventory/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def station(request, station):
+    context = {
+        'station':station,
+    }
+    return render(request, 'station/index.html', context=context)
+    
